29174fbcb61d6246e5f67ab38b41c0ae295ae986
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2020-02-24 19:36:36 +0300

    FSState: + tested loading sub directories

---
+++ test/fs/fs-state-load/subdirs-spec.ts
@@ -0,0 +1,71 @@
+import testLoadState, {ITestFiles} from "./testLoadState";
+
+describe("FSState", () => {
+
+    it("load sub dir", async() => {
+        const files: ITestFiles = {
+            "./sub/dir/test.sql": [
+                {
+                    type: "function",
+                    sql: `
+                        create function public.test()
+                        returns void as $body$
+                        begin
+                        end
+                        $body$
+                        language plpgsql;
+                    `,
+                    row: {
+                        identify: "public.test()",
+                        name: "test"
+                    }
+                }
+            ]
+        };
+
+        await testLoadState({
+            files,
+            expectedState: {
+                folder: {
+                    path: "./",
+                    name: "",
+                    files: [],
+                    folders: [
+                        {
+                            path: "./sub",
+                            name: "sub",
+                            files: [],
+                            folders: [
+                                {
+                                    path: "./sub/dir",
+                                    name: "dir",
+                                    files: [
+                                        {
+                                            path: "./sub/dir/test.sql",
+                                            name: "test.sql",
+                                            content: testLoadState.getFileSql( files["./sub/dir/test.sql"] )
+                                        }
+                                    ],
+                                    folders: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                functions: [
+                    {
+                        filePath: "./sub/dir/test.sql",
+                        identify: "public.test()",
+                        name: "test",
+                        parsed: null
+                    }
+                ],
+                triggers: [],
+                tables: [],
+                views: []
+            }
+        });
+
+    });
+    
+});
