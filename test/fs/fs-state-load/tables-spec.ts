78b4e38b1151d78e3a0a6fbfaa6115f86f5e0e9a
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2020-02-24 19:03:40 +0300

    added test for two tables inside one file

--- test/fs/fs-state-load/tables-spec.ts
+++ test/fs/fs-state-load/tables-spec.ts
@@ -77,4 +77,141 @@ describe("FSState", () => {
 
     });
     
+    
+    it("load dir with one file with two tables", async() => {
+        const files: ITestFiles = {
+            "./tables.sql": [
+                {
+                    type: "table",
+                    sql: `
+                        create table company (
+                            id serial primary key
+                        );
+                    `,
+                    row: {
+                        identify: "public.company",
+                        name: "company",
+                        columns: [
+                            {
+                                filePath: "./tables.sql",
+                                identify: "id",
+                                key: "id",
+                                type: "integer"
+                            }
+                        ],
+                        primaryKey: ["id"]
+                    }
+                },
+                {
+                    type: "table",
+                    sql: `
+                        create table orders (
+                            id serial primary key,
+                            id_company integer not null
+                        );
+                    `,
+                    row: {
+                        identify: "public.orders",
+                        name: "orders",
+                        columns: [
+                            {
+                                filePath: "./tables.sql",
+                                identify: "id",
+                                key: "id",
+                                type: "integer"
+                            },
+                            {
+                                filePath: "./tables.sql",
+                                identify: "id_company",
+                                key: "id_company",
+                                type: "integer",
+                                nulls: false
+                            }
+                        ],
+                        primaryKey: ["id"]
+                    }
+                }
+            ]
+        };
+
+        await testLoadState({
+            files,
+            expectedState: {
+                folder: {
+                    path: "./",
+                    name: "",
+                    files: [
+                        {
+                            path: "./tables.sql",
+                            name: "tables.sql",
+                            content: testLoadState.getFileSql(files["./tables.sql"])
+                        }
+                    ],
+                    folders: []
+                },
+                tables: [
+                    {
+                        filePath: "./tables.sql",
+                        identify: "public.company",
+                        parsed: null,
+                        name: "company",
+                        columns: [
+                            {
+                                filePath: "./tables.sql",
+                                identify: "id",
+                                key: "id",
+                                type: "integer",
+                                nulls: true,
+                                parsed: null
+                            }
+                        ],
+                        deprecated: false,
+                        deprecatedColumns: [],
+                        primaryKey: ["id"],
+                        checkConstraints: [],
+                        foreignKeysConstraints: [],
+                        uniqueConstraints: [],
+                        rows: null
+                    },
+                    {
+                        filePath: "./tables.sql",
+                        identify: "public.orders",
+                        parsed: null,
+                        name: "orders",
+                        columns: [
+                            {
+                                filePath: "./tables.sql",
+                                identify: "id",
+                                key: "id",
+                                type: "integer",
+                                nulls: true,
+                                parsed: null
+                            },
+                            {
+                                filePath: "./tables.sql",
+                                identify: "id_company",
+                                key: "id_company",
+                                type: "integer",
+                                nulls: false,
+                                parsed: null
+                            }
+                        ],
+                        deprecated: false,
+                        deprecatedColumns: [],
+                        primaryKey: ["id"],
+                        checkConstraints: [],
+                        foreignKeysConstraints: [],
+                        uniqueConstraints: [],
+                        rows: null
+                    }
+                ],
+                triggers: [],
+                functions: [],
+                views: []
+            }
+        });
+
+    });
+    
+    
 });
