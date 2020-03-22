2115f94abb9fb250f8d94ca56d0323b7f208b8b7
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2020-03-22 17:29:35 +0300

    removed trash

--- lib/state/FSDDLState.ts
+++ lib/state/FSDDLState.ts
@@ -1,14 +1,11 @@
-import DDLState, {TDBObject} from "./DDLState";
-import * as Path from "path";
+import DDLState from "./DDLState";
 
 // @see fs/index.ts
 import FolderModel from "../fs/FolderModel";
 import "../fs/FoldersCollection";
 
-import FSDriver from "../fs/FSDriver";
 import {Types} from "model-layer";
 import FileModel from "../fs/FileModel";
-import Parser from "../parser/Parser";
 
 export default class FSDDLState extends DDLState<FSDDLState> {
     structure() {
