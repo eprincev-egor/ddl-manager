33d4b91b636f5ac0e58655e2953ccf39e187c7e9
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2020-03-22 12:40:22 +0300

    more tests for parsing extension

--- test/Parser/PgParser/extension-spec.ts
+++ test/Parser/PgParser/extension-spec.ts
@@ -161,4 +161,226 @@ describe("PgParser", () => {
         ]);
     });
 
+    it("parse extension with unique constraint inside column", () => {
+        const parser = new PgParser();
+        
+        const result = parser.parseFile("test.sql", `
+            extension test
+            for order_type (
+                name text not null unique
+            )
+        `);
+
+        const extensionModel = result[0] as ExtensionModel;
+
+        assert.deepStrictEqual(extensionModel.toJSON().uniqueConstraints, [
+            {
+                filePath: "test.sql",
+                identify: "order_type_name_key", 
+                name: "order_type_name_key", 
+                unique: ["name"],
+                parsed: {
+                    name: null,
+                    column: {content: null, word: "name"},
+                    unique: [
+                        {content: null, word: "name"}
+                    ]
+                }
+            }
+        ]);
+    });
+    
+    it("parse extension with unique constraint inside body", () => {
+        const parser = new PgParser();
+        
+        const result = parser.parseFile("test.sql", `
+            extension test
+            for order_type (
+                constraint test_uniq unique (name)
+            )
+        `);
+
+        const extensionModel = result[0] as ExtensionModel;
+
+        assert.deepStrictEqual(extensionModel.toJSON().uniqueConstraints, [
+            {
+                filePath: "test.sql",
+                identify: "test_uniq", 
+                name: "test_uniq", 
+                unique: ["name"],
+                parsed: {
+                    name: {content: null, word: "test_uniq"},
+                    column: null,
+                    unique: [
+                        {content: null, word: "name"}
+                    ]
+                }
+            }
+        ]);
+    });
+
+    it("parse extension with check constraint at near column definition", () => {
+        const parser = new PgParser();
+        
+        const result = parser.parseFile("test.sql", `
+            extension test
+            for order_type (
+                name text check( name is not null )
+            )
+        `);
+
+        const extensionModel = result[0] as ExtensionModel;
+
+        assert.deepStrictEqual(extensionModel.toJSON().checkConstraints, [
+            {
+                filePath: "test.sql",
+                identify: "order_type_name_check", 
+                name: "order_type_name_check",
+                parsed: {
+                    name: null,
+                    column: {
+                        content: null,
+                        word: "name"
+                    },
+                    check: {
+                        elements: [
+                            {link: [{
+                                content: null, word: "name"
+                            }], star: false},
+
+                            {operator: "is not"},
+
+                            {null: true}
+                        ]
+                    }
+                }
+            }
+        ]);
+    });
+
+    it("parse table with check constraint inside body", () => {
+        const parser = new PgParser();
+        
+        const result = parser.parseFile("test.sql", `
+            extension test
+            for order_type ( 
+                constraint hello_name check( name is not null )
+            )
+        `);
+
+        const extensionModel = result[0] as ExtensionModel;
+
+        assert.deepStrictEqual(extensionModel.toJSON().checkConstraints, [
+            {
+                filePath: "test.sql",
+                identify: "hello_name", 
+                name: "hello_name",
+                parsed: {
+                    name: {
+                        content: null,
+                        word: "hello_name"
+                    },
+                    column: null,
+                    check: {
+                        elements: [
+                            {link: [{
+                                content: null, word: "name"
+                            }], star: false},
+
+                            {operator: "is not"},
+                            
+                            {null: true}
+                        ]
+                    }
+                }
+            }
+        ]);
+    });
+
+    it("parse extension with foreign key at near column definition", () => {
+        const parser = new PgParser();
+        
+        const result = parser.parseFile("test.sql", `
+            extension test
+            for companies (
+                country_id integer references countries
+            )
+        `);
+
+        const extensionModel = result[0] as ExtensionModel;
+
+        assert.deepStrictEqual(extensionModel.toJSON().foreignKeysConstraints, [
+            {
+                filePath: "test.sql",
+                identify: "companies_country_id_fkey", 
+                name: "companies_country_id_fkey",
+                columns: ["country_id"],
+                referenceColumns: ["id"],
+                referenceTableIdentify: "countries",
+                parsed: {
+                    name: null,
+                    column: {
+                        content: null,
+                        word: "country_id"
+                    },
+                    columns: [{
+                        content: null,
+                        word: "country_id"
+                    }],
+                    match: null,
+                    onDelete: null,
+                    onUpdate: null,
+                    referenceTable: {link: [
+                        {content: null, word: "countries"}
+                    ], star: false},
+                    referenceColumns: null
+                }
+            }
+        ]);
+    });
+
+    it("parse extension with foreign key inside body", () => {
+        const parser = new PgParser();
+        
+        const result = parser.parseFile("test.sql", `
+            extension test
+            for companies (
+                constraint country_fk 
+                    foreign key (country_id) 
+                    references countries
+            )
+        `);
+
+        const extensionModel = result[0] as ExtensionModel;
+
+        assert.deepStrictEqual(extensionModel.toJSON().foreignKeysConstraints, [
+            {
+                filePath: "test.sql",
+                identify: "country_fk", 
+                name: "country_fk",
+                columns: ["country_id"],
+                referenceColumns: ["id"],
+                referenceTableIdentify: "countries",
+                parsed: {
+                    name: {
+                        content: null,
+                        word: "country_fk"
+                    },
+                    column: null,
+                    columns: [{
+                        content: null,
+                        word: "country_id"
+                    }],
+                    match: null,
+                    onDelete: null,
+                    onUpdate: null,
+                    referenceTable: {link: [
+                        {content: null, word: "countries"}
+                    ], star: false},
+                    referenceColumns: null
+                }
+            }
+        ]);
+    });
+
 });
