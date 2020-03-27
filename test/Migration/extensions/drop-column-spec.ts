b796f230c5552bcb0e48db11940297b103c66ec0
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2020-03-27 19:43:56 +0300

    split extension tests

---
+++ test/Migration/extensions/drop-column-spec.ts
@@ -0,0 +1,66 @@
+import {testGenerateMigration} from "../testGenerateMigration";
+
+describe("MigrationController", () => {
+
+    describe("generateExtensions", () => {
+        
+        it("error on drop column, dev database", () => {
+            testGenerateMigration({
+                options: {
+                    mode: "dev"
+                },
+                fs: {
+                    extensions: [{
+                        filePath: "test_for_companies.sql",
+                        name: "test",
+                        identify: "extension test for public.companies",
+                        forTableIdentify: "public.companies",
+                        columns: []
+                    }],
+                    tables: [{
+                        filePath: "companies.sql",
+                        identify: "public.companies",
+                        name: "companies",
+                        columns: [{
+                            identify: "id",
+                            key: "id",
+                            type: "integer"
+                        }]
+                    }]
+                },
+                db: {
+                    tables: [{
+                        identify: "public.companies",
+                        name: "companies",
+                        columns: [
+                            {
+                                identify: "id",
+                                key: "id",
+                                type: "integer"
+                            },
+                            {
+                                identify: "name",
+                                key: "name",
+                                type: "text"
+                            }
+                        ]
+                    }]
+                },
+                migration: {
+                    commands: [],
+                    errors: [
+                        {
+                            filePath: "companies.sql",
+                            code: "CannotDropColumnError",
+                            message: "cannot drop column public.companies.name, please use deprecated section",
+                            tableIdentify: "public.companies",
+                            columnKey: "name"
+                        }
+                    ]
+                }
+            });
+        });
+
+    });
+    
+});
---
+++ test/Migration/extensions/unknown-table-error-spec.ts
@@ -0,0 +1,37 @@
+import {testGenerateMigration} from "../testGenerateMigration";
+
+describe("MigrationController", () => {
+
+    describe("generateExtensions", () => {
+        
+        it("error on unknown table", () => {
+            testGenerateMigration({
+                fs: {
+                    extensions: [{
+                        filePath: "test_for_companies.sql",
+                        name: "test",
+                        identify: "extension test for public.companies",
+                        forTableIdentify: "public.companies"
+                    }]
+                },
+                db: {
+                    functions: []
+                },
+                migration: {
+                    commands: [],
+                    errors: [
+                        {
+                            filePath: "test_for_companies.sql",
+                            code: "UnknownTableForExtensionError",
+                            message: "table public.companies does not exists for extension test",
+                            extensionName: "test",
+                            tableIdentify: "public.companies"
+                        }
+                    ]
+                }
+            });
+        });
+        
+    });
+
+});
