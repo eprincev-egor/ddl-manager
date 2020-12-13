771e65e34e55c0c6f99c302f5cda49b49c5e3472
Eprincev.Egor <eprincev.egor@ya.ru>
2020-12-13 17:01:14 +0300

    refactor

--- lib/database/postgres/helper-functions/index.ts
+++ lib/database/postgres/helper-functions/index.ts
@@ -4,14 +4,6 @@ export const CM_ARRAY_REMOVE_ONE_ELEMENT = fs.readFileSync(
     __dirname + "/cm_array_remove_one_element.sql"
 ).toString();
 
-export const CM_ARRAY_TO_STRING_DISTINCT = fs.readFileSync(
-    __dirname + "/cm_array_to_string_distinct.sql"
-).toString();
-
-export const CM_DISTINCT_ARRAY = fs.readFileSync(
-    __dirname + "/cm_distinct_array.sql"
-).toString();
-
 export const CM_EQUAL_ARRAYS = fs.readFileSync(
     __dirname + "/cm_equal_arrays.sql"
 ).toString();
