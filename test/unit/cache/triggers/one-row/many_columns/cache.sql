5efb46e66b8ffe524f4fd1fe8745379e7ad99830
Eprincev.Egor <eprincev.egor@ya.ru>
2021-03-04 11:48:20 +0300

    one-row trigger: + test for many columns

---
+++ test/unit/cache/triggers/one-row/many_columns/cache.sql
@@ -0,0 +1,9 @@
+cache parent_row for operation.operation as child_oper (
+    select
+        parent_oper.id_order as parent_id_order,
+        coalesce(parent_oper.lvl, 0) + 1 as lvl
+
+    from operation.operation as parent_oper
+    where
+        parent_oper.id = child_oper.id_parent_operation
+)
---
+++ test/unit/cache/triggers/one-row/many_columns/cache_parent_lvl_for_operation_on_operation.sql
@@ -0,0 +1,63 @@
+create or replace function cache_parent_row_for_operation_on_operation()
+returns trigger as $body$
+begin
+
+    if TG_OP = 'DELETE' then
+        if
+            old.id_order is not null
+            or
+            coalesce(old.lvl, 0) + 1 is not null
+        then
+            update operation.operation as child_oper set
+                parent_id_order = null,
+                lvl = coalesce(null, 0) + 1
+            where
+                old.id = child_oper.id_parent_operation;
+        end if;
+
+        return old;
+    end if;
+
+    if TG_OP = 'UPDATE' then
+        if
+            new.id_order is not distinct from old.id_order
+            and
+            new.lvl is not distinct from old.lvl
+        then
+            return new;
+        end if;
+
+        update operation.operation as child_oper set
+            parent_id_order = new.id_order,
+            lvl = coalesce(new.lvl, 0) + 1
+        where
+            new.id = child_oper.id_parent_operation;
+
+        return new;
+    end if;
+
+    if TG_OP = 'INSERT' then
+        if
+            new.id_order is not null
+            or
+            coalesce(new.lvl, 0) + 1 is not null
+        then
+            update operation.operation as child_oper set
+                parent_id_order = new.id_order,
+                lvl = coalesce(new.lvl, 0) + 1
+            where
+                new.id = child_oper.id_parent_operation;
+        end if;
+
+        return new;
+    end if;
+
+end
+$body$
+language plpgsql;
+
+create trigger cache_parent_row_for_operation_on_operation
+after insert or update of id_order, lvl or delete
+on operation.operation
+for each row
+execute procedure cache_parent_row_for_operation_on_operation();
